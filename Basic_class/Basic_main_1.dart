a607157aa495a104c35b5e40499552ea7592d8a9
sloving <sayandev302775@gmail.com>
2024-03-07 01:18:04 +0600

    Now you can go

---
+++ Basic_class/Basic_main_1.dart
@@ -0,0 +1,14 @@
+main() {
+  print('Hello world this is my first code'); ///This is a statement;
+  int a = 34;
+  int b = 36;
+  int c = a + b;
+  double Money = 23435.45;
+  String Name ='Sayan dev';
+  print(c);
+  print(Money);
+  print(Name);
+
+  print(134 + 456);
+  print('h');
+}
---
+++ OOP/exception_handling.dart
@@ -0,0 +1,4 @@
+void main() {
+  int number = 01632237676;
+  print('number');
+}
